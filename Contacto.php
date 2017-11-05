74d8c8f81924ef0dfe24e27c2038523b9dfb5820
raul4h <rjhinostrozagarcia@miners.utep.edu>
2017-11-05 13:07:06 -0700

    contact

--- Contacto.php
+++ Contacto.php
@@ -16,14 +16,22 @@
  		height: 100%;
  	}
 
-		.container {
-			width: 30%;
-		}	
-		li{
-			font-family: helvetica;
-			font-weight: lighter;
-			font-size: 50%;
-		}	
+ 	.container {
+ 		width: 30%;
+ 	}	
+ 	li{
+ 		font-family: helvetica;
+ 		font-weight: lighter;
+ 		font-size: 50%;
+ 	}	
+ 	.input-field label {
+ 		color: white	;
+ 	}
+ 	#map {
+ 		height: 400px;
+ 		background-color: grey;
+ 	}
+
  </style>
 
 </head>
@@ -63,19 +71,63 @@
 	</nav>
 	
 
-	
-	
-	<!--Import jQuery before materialize.js-->
-	<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+	<div class="row">
+		<div class="col s7 offset-s1">
+			<form method="GET">
+				<div class="col s5">
+					<div class="input-field col s12">
+						<input placeholder="Nombre" id="Nombre" name="nombre" required type="text" class="validate white-text">
+					</div>
+					<div class="input-field col s12">
+						<input placeholder="Telefono" id="Telefono" name="telefono" type="text" class="validate white-text">
+					</div>
+					<div class="input-field col s12">
+						<input placeholder="Tu Correo" id="Correo" name="email" type="email" required class="validate white-text">
+					</div>
+				</div>
+				<div class="col s7">
+					<div class="input-field col s12">
+						<textarea id="textarea1" name="comentarios" required class="materialize-textarea white-text"></textarea>
+						<label for="textarea1">Comentarios</label>
+						<button class="btn waves-effect waves-light red" style="width: 100%" type="submit">Enviar</button>
+					</div>
+				</div>
+			</form>
+		</div>
+		<div class="col s4">
 
-	<script type="text/javascript">
+		</div>
+		<div class="col s8" id="map">
+		</div>
+	</div>
+</div>
+
+<!--Import jQuery before materialize.js-->
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+
+<script type="text/javascript">
 	// Initialize collapse button
 	$(".button-collapse").sideNav();
   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
   //$('.collapsible').collapsible();
   
 </script>
+<script>
+	var map;
+	function initMap() {
+		var uluru = {lat: 31.7510324, lng: -106.4496282};
+		var map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 15,
+			center: uluru
+		});
+		var marker = new google.maps.Marker({
+			position: uluru,
+			map: map
+		});
+	}
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDttv_N5SBTmrEZ66Enlx-13yP9UoqDrCI&callback=initMap" async defer></script>
 
 
 </body>
